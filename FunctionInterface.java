8788fdafebde4f0b06540f8bd6767caf715139c9
Mrqinforce <1293340422@qq.com>
2020-02-23 15:06:50 +0800

    秦健的代码

---
+++ FunctionInterface.java
@@ -0,0 +1,13 @@
+package FunctionalInterance;
+
+/**
+ * @ClassName $(Name)
+ * @Description TODO
+ * @Author qin jian
+ * @Date 2020/2/22
+ * @Version 1.0
+ **/
+@FunctionalInterface
+public interface FunctionInterface {
+    void inform(String message);
+}

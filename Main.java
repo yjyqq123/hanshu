5ec5589efe386efa1c7a88d07c2ae45648ad72b8
事在人为 <1914866205@qq.com>
2020-02-23 09:56:17 +0800

    倪涛涛的代码

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
+        FunctionInterfaceTest functionInterfaceTest= food -> {
+            System.out.println("我爱吃" + food);
+        };
+        functionInterfaceTest.eat("六食堂黄焖鸡");
+
         Consumer consumer= o -> System.out.println("接收Object对象但不返回值");
         consumer.accept(consumer);
 
@@ -17,5 +22,27 @@ public class Main {
         int i = (int) privilegedAction.run();
         System.out.println(i);
 
+        Consumer consumer1= o -> {
+                  String str= (String) o;
+                  //把所有单词按照空格分隔成一个数组
+                  String [] arr=str.split(" ");
+                  //把单词按照长度排序
+            for (int j = 0; j <arr.length-1 ; j++) {
+                for (int k = 0; k < arr.length-1; k++) {
+                    if (arr[k].length()<arr[k+1].length()){
+                        String temp=arr[k+1];
+                        arr[k+1]=arr[k];
+                        arr[k]=temp;
+                    }
+                }
+            }
+            for (int j = 0; j <arr.length ; j++) {
+                System.out.println(arr[j]);
+                if (j==4){
+                    break;
+                }
+            }
+        };
+        consumer1.accept("ad js mm qi aaa abs adas asasa mka");
     }
 }
